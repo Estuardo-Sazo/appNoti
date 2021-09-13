14e3499bb5d0b5503e64cb3ca7bde36924515e1d
Estuardo-sazo <estuardosazo1206@gmail.com>
2021-09-13 00:15:59 -0600

    Inicion ventana de login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,17 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'main',
     loadChildren: () => import('./pages/tabs/tabs.module').then(m => m.TabsPageModule)
   },
   {
     path: 'login',
     loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path:'',
+    pathMatch:'full',
+    redirectTo:'login'
   }
 ];
 @NgModule({
--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
+  avatars = [
+    {
+      img: 'av-1.png',
+      seleccionado: true
+    },
+    {
+      img: 'av-2.png',
+      seleccionado: false
+    },
+    {
+      img: 'av-3.png',
+      seleccionado: false
+    },
+    {
+      img: 'av-4.png',
+      seleccionado: false
+    },
+    {
+      img: 'av-5.png',
+      seleccionado: false
+    },
+    {
+      img: 'av-6.png',
+      seleccionado: false
+    },
+    {
+      img: 'av-7.png',
+      seleccionado: false
+    },
+    {
+      img: 'av-8.png',
+      seleccionado: false
+    },
+];
+
+avatarSlide={
+  slidesPerView:3.5
+}
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  login(fLogin:NgForm){
+    console.log(fLogin.valid);
+
+  }
+
+  registr(fRegistro:NgForm){
+    console.log(fRegistro.valid);
+
+  }
+
+  seleccionarAvatar(avatar){
+    this.avatars.forEach(av=>av.seleccionado=false);
+    avatar.seleccionado= true
+  }
 }
